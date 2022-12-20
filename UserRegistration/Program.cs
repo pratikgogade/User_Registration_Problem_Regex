234dfd4989a1960e1804944d74188bb4d4ebc47f
unknown <Pratik@Example.com>
2022-12-20 20:33:56 +0530

    [Pratik]Add:Valid First Name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;//namespace for regex
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("User Registration Problem using REGEX");
+                Console.WriteLine("1.First Name");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        FirstName firstName = new FirstName();
+                        firstName.First();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+
+    }
+}
